15e3dd44c8be2ff15321e76eb0933a54e9fedbd6
SunGod7 <sun7ruler@gmail.com>
2022-07-28 15:27:53 -0400

    was working at one point

---
+++ src/FilmPoster.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function FilmPoster() {
+    return (
+        <img src={this.props.poster_path} 
+            alt={`Poster of the film ${this.props.title}`} />
+            )
+    }
+
+    export default FilmPoster
